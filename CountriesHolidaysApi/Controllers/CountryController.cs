a37b1adb2ca984407e888ecd6becb7a782a96970
Ahmed <Ahmed@DESKTOP-RLAOTG8>
2023-01-09 11:52:20 +0200

    Add project files.

---
+++ CountriesHolidaysApi/Controllers/CountryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CountriesHolidaysApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+
+    }
+}
---
+++ CountriesHolidaysApi/Models/Holiday.cs
@@ -0,0 +1,11 @@
+﻿namespace CountriesHolidaysApi.Models
+{
+    public class Holiday
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int CountryId { get; set; }
+    }
+    }
+}
